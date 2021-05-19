89209114d984e5b31eb038b468207f621a2f763b
Vera van Klaveren <edo@van-klaveren.com>
2021-05-20 01:32:18 +0200

    Added all the code and assets of the proof-of-concept project.

---
+++ src/CMS/CMSAdminControls/CKeditor/plugins/inlineeditableareas/plugin.js
@@ -0,0 +1,19 @@
+﻿CKEDITOR.plugins.add('inlineeditableareas', {
+    icons: 'inlineeditableareas',
+    init: function (editor) {
+        editor.addCommand('insertEditableArea', {
+            exec: function (editor) {
+                var editableAreaId = Math.random().toString(36).substring(2);
+                var html = '<editable-area id="' + editableAreaId + '"></editable-area>';
+                var editableArea = CKEDITOR.dom.element.createFromHtml(html);
+
+                editor.insertElement(editableArea);
+            }
+        });
+        editor.ui.addButton('inlineeditableareas', {
+            label: 'Insert widget area',
+            command: 'insertEditableArea',
+            toolbar: 'insert'
+        });
+    }
+});
---
+++ src/MVC/Kentico/InlineEditableAreas.cs
@@ -0,0 +1,53 @@
+﻿using Kentico.Web.Mvc;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Kentico.PageBuilder.Web.Mvc
+{
+    /// <summary>
+    /// A static class implementing operations for working with inline editable areas.
+    /// </summary>
+    public static class InlineEditableAreas
+    {
+        /// <summary>
+        /// Resolves the editable areas in the given HTML string and writes the result to the view context.
+        /// </summary>
+        /// <param name="htmlHelper">An <see cref="IHtmlHelper"/> object containing the view context.</param>
+        /// <param name="html">The HTML string.</param>
+        /// <returns>The return value is not used and will always be null.</returns>
+        public static async Task<object> ResolveEditableAreasAsync(
+            this IHtmlHelper htmlHelper,
+            string html)
+        {
+            var components = Regex.Split(html, @"<editable-area id=""(.*?)""><\/editable-area>");
+            for (var i = 0; i < components.Length; i++)
+            {
+                var isEditableArea = i % 2 == 1;
+                if (isEditableArea)
+                {
+                    await RenderEditableAreaAsync(components[i]);
+                }
+                else
+                {
+                    RenderHtml(components[i]);
+                }
+            }
+
+            return null;
+
+            async Task RenderEditableAreaAsync(string identifier)
+            {
+                var kentico = htmlHelper.Kentico();
+                RenderContent(await kentico.EditableAreaAsync(identifier));
+            }
+
+            void RenderHtml(string value) =>
+                RenderContent(content: htmlHelper.Raw(value));
+
+            void RenderContent(IHtmlContent content) =>
+                htmlHelper.ViewContext.Writer.Write(content);
+        }
+    }
+}
